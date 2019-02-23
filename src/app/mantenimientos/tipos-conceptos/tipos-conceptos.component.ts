2625fc9157430ea867a58edda6b8dd535add71ff
AntoniSirena <lic-juansirena@hotmail.com>
2019-02-22 21:16:17 -0400

    se isntalo la libreria de dvkit

--- src/app/mantenimientos/tipos-conceptos/tipos-conceptos.component.ts
+++ src/app/mantenimientos/tipos-conceptos/tipos-conceptos.component.ts
@@ -26,7 +26,7 @@ export class TiposConceptosComponent implements OnInit, OnDestroy  {
 
   tiposConceptos: any[] = [];
   tiposGastos : any[] = [];
-  tipoConcepto: any = {Id:'',Codigo:'' ,Descripcion:'', TipoGastoId:''};
+  tipoConcepto: any = {Id:'',Codigo:'' ,Descripcion:'', tipoGastoId:''};
 
 
  //Primer metodo que se ejecuta al momento de cargar el sistema
